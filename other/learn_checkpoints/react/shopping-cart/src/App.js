634e878f2b97928709daaa6dc64ef50015e0e634
JBallin <JBallin@users.noreply.github.com>
2018-10-29 12:53:52 -0700

    Add CartHeader

--- other/learn_checkpoints/react/shopping-cart/src/App.js
+++ other/learn_checkpoints/react/shopping-cart/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import CartHeader from './components/CartHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Shopping Cart
-          </p>
+          <CartHeader />
         </header>
       </div>
     );
