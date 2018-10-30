1a9a79c166cce687c81022ec0befd2375c90c0bc
JBallin <JBallin@users.noreply.github.com>
2018-10-30 12:57:32 -0700

    Style cart header

--- other/learn_checkpoints/react/shopping-cart/src/components/CartItems.jsx
+++ other/learn_checkpoints/react/shopping-cart/src/components/CartItems.jsx
@@ -4,11 +4,11 @@ import CartItem from './CartItem.jsx';
 const CartItems = ({ cartItems, deleteItem }) => (
   <div className="container mt-5">
     <div className="list-group">
-      <div className="list-group-item">
+      <div className="list-group-item bg-dark text-white">
         <div className="row">
-          <div className="col-md-7 font-weight-bold">Product</div>
-          <div className="col-md-2 font-weight-bold">Price</div>
-          <div className="col-md-2 font-weight-bold">Quantity</div>
+          <div className="col-md-7">Product</div>
+          <div className="col-md-2">Price</div>
+          <div className="col-md-2">Quantity</div>
         </div>
       </div>
       { cartItems.map(item => <CartItem key={item.id} item={item} deleteItem={deleteItem} />) }
