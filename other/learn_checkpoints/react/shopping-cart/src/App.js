7b054728e37822936078ceb795b59dab9f1a10ab
JBallin <JBallin@users.noreply.github.com>
2018-10-31 12:50:52 -0700

    Store static products list in separate file

--- other/learn_checkpoints/react/shopping-cart/src/App.js
+++ other/learn_checkpoints/react/shopping-cart/src/App.js
@@ -4,20 +4,10 @@ import CartHeader from './components/CartHeader.jsx';
 import CartItems from './components/CartItems.jsx';
 import AddItem from './components/AddItem.jsx';
 import CartFooter from './components/CartFooter.jsx';
+import products from './db/products';
 
 class App extends Component {
   state = {
-    products: [
-      { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 },
-      { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 },
-      { id: 42, name: 'Intelligent Paper Knife', priceInCents: 1999 },
-      { id: 43, name: 'Small Aluminum Keyboard', priceInCents: 2500 },
-      { id: 44, name: 'Practical Copper Plate', priceInCents: 1000 },
-      { id: 45, name: 'Awesome Bronze Pants', priceInCents: 399 },
-      { id: 46, name: 'Intelligent Leather Clock', priceInCents: 2999 },
-      { id: 47, name: 'Ergonomic Bronze Lamp', priceInCents: 40000 },
-      { id: 48, name: 'Awesome Leather Shoes', priceInCents: 3990 },
-    ],
     cartItemsList: [
       { id: 1, product: { id: 40, name: 'Mediocre Iron Watch', priceInCents: 399 }, quantity: 1 },
       { id: 2, product: { id: 41, name: 'Heavy Duty Concrete Plate', priceInCents: 499 }, quantity: 2 },
