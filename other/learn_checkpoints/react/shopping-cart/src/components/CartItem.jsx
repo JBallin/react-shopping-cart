6f85654b6d89cedd0ae1a195787daf6d6661a9af
JBallin <JBallin@users.noreply.github.com>
2018-11-01 16:29:17 -0700

    Show price instead of cost in cartItems

--- other/learn_checkpoints/react/shopping-cart/src/components/CartItem.jsx
+++ other/learn_checkpoints/react/shopping-cart/src/components/CartItem.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { toDollars} from '../utils/formatCurrency';
 
 const CartItem = ({ item, removeFromCart, product }) => {
-  const costInCents = product.priceInCents * item.quantity;
-  const cost = toDollars(costInCents/100);
 
   return (
     <div className="list-group-item">
       <div className="row">
         <div className="col-md-7">{ product.name }</div>
-        <div className="col-md-2">{ cost }</div>
+        <div className="col-md-2">{ toDollars(product.priceInCents/100) }</div>
         <div className="col-md-2">{ item.quantity }</div>
         <button className="col-md-1 fa fa-trash btn" aria-hidden="true" onClick={() => removeFromCart(item)}>
         </button>
